18dba2baebf0ed80080599175781d96fe20f8815
adminstratserver <admin@optionnalytics.com>
2019-12-24 17:53:50 +0800

    new installpackage playbook

---
+++ ansible/run.sh
@@ -0,0 +1,3 @@
+ansible-playbook F_installpackages.yaml -i inventry.txt -u djangoadmin16
+#ansible-playbook E_pullfilesfromserver.yaml -i inventry.txt -u djangoadmin16
+#ansible-playbook C_softwareupdate.yaml -i inventry.txt -u djangoadmin16
--- ansible/run_playbook.sh
+++ ansible/run_playbook.sh
@@ -2,3 +2,5 @@ ansible-playbook A_createnewuser.yaml -i inventry.txt -u root
 ansible-playbook B_serversecurity.yaml -i inventry.txt -u root
 ansible-playbook C_softwareupdate.yaml -i inventry.txt -u djangoadmin16
 ansible-playbook D_createdatabase.yaml -i inventry.txt -u djangoadmin16
+ansible-playbook E_pullfilesfromserver.yaml -i inventry.txt -u djangoadmin16
+ansible-playbook F_installpackages.yaml -i inventry.txt -u djangoadmin16
