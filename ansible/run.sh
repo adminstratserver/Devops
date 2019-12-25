7229da5af45880db5407fac14e91c097583db671
adminstratserver <admin@optionnalytics.com>
2019-12-25 16:07:04 +0800

    change to F_installpackages.yaml to include gunicorn changes

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,2 +1,7 @@
-ansible-playbook G_djangomigrate.yaml -i inventry.txt -u djangoadmin16
-#ansible-playbook F_installpackages.yaml -i inventry.txt -u djangoadmin16
+#ansible-playbook A_createnewuser.yaml -i inventry.txt -u root
+#ansible-playbook B_serversecurity.yaml -i inventry.txt -u root
+#ansible-playbook C_softwareupdate.yaml -i inventry.txt -u djangoadmin16
+#ansible-playbook D_createdatabase.yaml -i inventry.txt -u djangoadmin16
+#ansible-playbook E_pullfilesfromserver.yaml -i inventry.txt -u djangoadmin16
+ansible-playbook F_installpackages.yaml -i inventry.txt -u djangoadmin16
+#ansible-playbook G_djangomigrate.yaml -i inventry.txt -u djangoadmin16
